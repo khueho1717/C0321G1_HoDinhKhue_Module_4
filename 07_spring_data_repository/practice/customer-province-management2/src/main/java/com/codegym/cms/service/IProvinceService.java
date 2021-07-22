5532bedbaff22d595227ec33f28a3129fd770638
hyuganejidn@gmail.com <xuan hung>
2021-07-22 13:08:15 +0700

    07_th

---
+++ 07_spring_data_repository/practice/customer-province-management2/src/main/java/com/codegym/cms/service/IProvinceService.java
@@ -0,0 +1,9 @@
+package com.codegym.cms.service;
+
+import com.codegym.cms.model.Customer;
+import com.codegym.cms.model.Province;
+import com.codegym.cms.service.IGeneralService;
+
+public interface IProvinceService extends IGeneralService<Province> {
+
+}
