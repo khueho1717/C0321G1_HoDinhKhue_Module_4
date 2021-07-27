bf077c939242138221afc3c1052763c3234cb1df
hyuganejidn@gmail.com <xuan hung>
2021-07-28 06:43:46 +0700

    09_thuc_hanh

---
+++ 09_ aop_exception_handling/practice/exception_handle_customer/src/main/java/com/example/aspect_log/exception/DuplicateEmailException.java
@@ -0,0 +1,4 @@
+package com.example.aspect_log.exception;
+
+public class DuplicateEmailException extends Exception{
+}
---
+++ 09_ aop_exception_handling/practice/exception_handle_customer/src/main/java/com/example/aspect_log/repository/ProvinceRepository.java
@@ -0,0 +1,9 @@
+package com.example.aspect_log.repository;
+
+import com.example.aspect_log.model.Province;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProvinceRepository extends CrudRepository<Province, Long> {
+}
